e77848034d36ebf7424c74e633bdf7e92199c2d1
Raymond McCrae <raymccrae@yahoo.com>
2018-11-20 21:57:50 +0000

    Add Bundle extension for unit tests

---
+++ Tests/JSONPatchTests/Bundle.swift
@@ -0,0 +1,13 @@
+//
+//  Bundle.swift
+//  JSONPatchTests
+//
+//  Created by Raymond Mccrae on 19/11/2018.
+//  Copyright © 2018 Raymond McCrae. All rights reserved.
+//
+
+import Foundation
+
+extension Bundle {
+    static let test = Bundle(identifier: "scot.raymccrae.JSONPatchTests")!
+}
