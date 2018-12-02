520ad50bcb8580da765a114dbe172acbe6f76d21
Raymond McCrae <raymccrae@yahoo.com>
2018-12-02 19:25:41 +0000

    Fix cyclic dependency in Package.swift

--- Package.swift
+++ Package.swift
@@ -20,7 +20,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "JSONPatch",
-            dependencies: ["JSONPatch"]),
+            dependencies: []),
         .testTarget(
             name: "JSONPatchTests",
             dependencies: ["JSONPatch"],
