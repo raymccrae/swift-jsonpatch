115b1810fd02a48f5653436d8a169df58b6bd65b
Cassiano Monteiro <cmonteiro@dialpad.com>
2019-08-22 17:34:05 -0400

    Carthage compatibility.
    Renamed header.

---
+++ JSONPatchFramework/JSONPatch.h
@@ -0,0 +1,19 @@
+//
+//  JSONPatch.h
+//  JSONPatch
+//
+//  Created by Cassiano Monteiro on 2019-08-22.
+//  Copyright © 2019 Raymond McCrae. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for JSONPatchFramework.
+FOUNDATION_EXPORT double JSONPatchVersionNumber;
+
+//! Project version string for JSONPatchFramework.
+FOUNDATION_EXPORT const unsigned char JSONPatchVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JSONPatch/PublicHeader.h>
+
+#import <JSONPatch/JSONPatch.h>
